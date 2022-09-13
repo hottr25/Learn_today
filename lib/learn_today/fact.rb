696fb1946ebefb0b50e5aff82b1fca4413607997
Rob Hott <Robhott48@gmail.com>
2022-09-12 20:22:03 -0400

    added httparty

---
+++ lib/learn_today/fact.rb
@@ -0,0 +1,13 @@
+# this class is used to retrieve fact of the day
+#https://asli-fun-fact-api.herokuapp.com/
+# {
+#     "status":true,
+#     "data":{
+#         "id":"17",
+#         "fact":"The severed head of a sea slug can grow a whole new body.",
+#         "cat":"sea",
+#         "hits":"1858"}
+#     }
+class Fact
+
+end
